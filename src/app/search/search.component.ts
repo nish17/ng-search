b37efe61a1b73e158366fc477f6dfa820f153425
Nimesh Solanki <nimesh3536@gmail.com>
2020-01-16 16:46:58 +0530

    add formArray and control array

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import { CategoryService } from '../category.service';
+import { FormControl, FormBuilder, FormGroup, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,15 +8,30 @@ import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class SearchComponent implements OnInit {
   @Input() categories;
-  constructor() { }
-  SearchForm = new FormGroup({
-    search: new FormControl(''),
-    category: new FormControl('')
-  });
-  ngOnInit() {
-  }
+  fetchedCategories: string[];
+  SearchForm: FormGroup;
+  searchTerm: string;
+  controlArr: FormControl[];
+  formArr: FormArray;
+  constructor(private categoryService: CategoryService, private fb: FormBuilder) { }
 
-  onCheckboxChange(category: string) {
-    console.log(category);
+  ngOnInit() {
+    this.SearchForm = this.fb.group({
+      search: new FormControl(''),
+      categoryfilters: this.formArr
+      // new FormArray(this.controlArr)
+    });
+    this.getCategories();
+    this.SearchForm.valueChanges.subscribe(console.log);
+  }
+  getCategories() {
+    this.categoryService.getCategories().subscribe(categories => {
+      this.fetchedCategories = categories;
+      // this.controlArr = [new FormArray(new Array(this.fetchedCategories.length).fill(new FormControl('false')))];
+      this.controlArr = new Array(this.fetchedCategories.length).fill(new FormControl('false'));
+      this.formArr = new FormArray(this.controlArr);
+      console.log(this.controlArr);
+      console.log(this.formArr);
+    });
   }
 }
