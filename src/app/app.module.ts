18e1442e1a2e7854b03652e62f48b3ccd3e96c4d
Nimesh Solanki <nimesh3536@gmail.com>
2020-01-17 17:00:26 +0530

    add mat-components and infiniteschoolModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule, MatCheckboxModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ResultFeedComponent } from './result-feed/result-feed.component';
-
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +26,10 @@ import { ResultFeedComponent } from './result-feed/result-feed.component';
     MatCardModule,
     MatCheckboxModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule, 
+    MatDividerModule,
+    MatListModule,
+    InfiniteScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
