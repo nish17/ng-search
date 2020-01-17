f567cd22cf919dc6b5ceb72ea0e17fff1c6b3799
Nimesh Solanki <nimesh3536@gmail.com>
2020-01-17 15:01:49 +0530

    update result-feed component to fetch the changes in search

--- src/app/result-feed/result-feed.component.ts
+++ src/app/result-feed/result-feed.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FetchDataService } from '../fetch-data.service';
+import { Component, OnInit, Input, SimpleChanges, SimpleChange } from '@angular/core';
+// import { FetchDataService } from '../fetch-data.service';
+import {SearchComponent} from '../search/search.component';
 @Component({
   selector: 'app-result-feed',
   templateUrl: './result-feed.component.html',
@@ -7,9 +8,14 @@ import { FetchDataService } from '../fetch-data.service';
 })
 export class ResultFeedComponent implements OnInit {
   @Input() results: string[] = [];
-  constructor(private fetchDataService: FetchDataService) { }
+  @Input() searchTerm: string;
+  constructor() {}
 
   ngOnInit() {
+    console.log(`Inside result-feed component==>${JSON.stringify(this.results)}`);
   }
-
+  ngOnChanges(changes: SimpleChanges){
+    this.results = changes.results.currentValue;
+    // console.log(changes);
+  }
 }
