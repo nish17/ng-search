a793fbdbb7790e9e4cdb35376697d3b730728490
Nimesh Solanki <nimesh3536@gmail.com>
2020-01-17 12:40:43 +0530

    add new service to fetch movies based on search from the backend

---
+++ src/app/fetch-data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap, pluck } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchDataService {
+  private categoryURL = 'https://movie-search-project.herokuapp.com//?format=json';
+  private searchURL = 'https://movie-search-project.herokuapp.com/movie?q=';
+  constructor(private http: HttpClient) { }
+
+  getMovies(searhTerm: string){
+    return this.http.get<object[]>(this.searchURL).pipe(pluck('results'));
+  }
+}
