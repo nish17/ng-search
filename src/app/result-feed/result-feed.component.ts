a4fea454aab650f5fb946c8d8418b37663e4c87c
Nimesh Solanki <nimesh3536@gmail.com>
2020-01-18 23:56:38 +0530

    update logic for infinite scroll functionality

--- src/app/result-feed/result-feed.component.ts
+++ src/app/result-feed/result-feed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, SimpleChanges, SimpleChange } from '@angular/core';
-import { SearchComponent } from '../search/search.component';
+import { FetchDataService } from '../fetch-data.service';
+import { debounceTime } from 'rxjs/operators';
 
 
 @Component({
@@ -8,13 +9,13 @@ import { SearchComponent } from '../search/search.component';
   styleUrls: ['./result-feed.component.css']
 })
 export class ResultFeedComponent implements OnInit {
-  @Input() results;
+  @Input() results: [string];
   @Input() previousURL: string;
   @Input() nextURL: string;
   // private nextURL: string = this.results.next;
   // private previousURL:string = this.results.previous;
 
-  constructor() { }
+  constructor(private fetchDataService: FetchDataService) { }
 
   ngOnInit() {
     // console.log(`Inside result-feed component==>${JSON.stringify(this.results)}`);
@@ -22,6 +23,17 @@ export class ResultFeedComponent implements OnInit {
   }
   onScroll() {
     console.log('Scrolled');
-    console.log(this.previousURL, this.nextURL);
+    // console.log(`this.results=====>${JSON.stringify(this.results)}`);
+    // console.log(`previousURL: ${this.previousURL}`, `nextURL: ${this.nextURL}`);
+    if (this.nextURL) {
+      this.fetchDataService.fetchMovies(this.nextURL).pipe(debounceTime(200)).subscribe((movies) => {
+        // this.results.push.apply(movies['results'], movies['results']);
+        const newMovies = movies['results'];
+        this.results.push(...newMovies);
+        this.nextURL = movies['next'];
+        this.previousURL = movies['previous'];
+      });
+    }
+    // this.fetchDataService.fetchMovies(this.nextURL).subscribe((movies: any) => { this.results = [...this.results, ...movies.results]; });
   }
 }
